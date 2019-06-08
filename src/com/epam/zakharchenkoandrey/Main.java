b50c4d4bc33d9ba142adf9ae72e4fb21cb076c7a
andreyzakharchenko2019 <andruha.zakharchenko@gmail.com>
2019-06-08 11:29:04 +0600

    The first download project on GutHub.

---
+++ src/com/epam/zakharchenkoandrey/Main.java
@@ -0,0 +1,33 @@
+package com.epam.zakharchenkoandrey;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // A1. В  каждом  слове  текста  k-ю  букву  заменить  заданным  символом.
+        // Если k больше длины слова, корректировку не выполнять.
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter some sentence: ");
+        String someSentence = reader.readLine();
+        System.out.print("What letter do you want to change (int)? ");
+        int kLatter = Integer.parseInt(reader.readLine());
+        System.out.print("Enter the desired letter: ");
+        String needLatter = reader.readLine();
+
+        if (kLatter > someSentence.length()) {
+            System.out.println("Number is greater than the length of the string");
+        }
+        else {
+            char oldChar = someSentence.charAt(kLatter);
+            char newChar = needLatter.charAt(0);
+            System.out.println(someSentence.replace(oldChar, newChar));
+        }
+
+
+    }
+}
