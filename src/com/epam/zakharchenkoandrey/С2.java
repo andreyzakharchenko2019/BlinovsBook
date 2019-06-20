b3214a6f6957b675c31d27291cac3fa58a604f94
andreyzakharchenko2019 <andruha.zakharchenko@gmail.com>
2019-06-20 13:26:33 +0600

    The first download project on GitHub.

--- src/com/epam/zakharchenkoandrey/С2.java
+++ src/com/epam/zakharchenkoandrey/С2.java
@@ -16,27 +16,33 @@ public class С2 {
         System.out.print("Enter some sentence: ");
         String someSentence = reader.readLine();
         String[] listSentence = someSentence.split("");
-        List<String> arrayLisrSentence = Arrays.asList(listSentence);
-        List<Integer> listIndex = new ArrayList<>();
         Map<Integer, Integer> mapIndex = new HashMap<>();
         for (int i = 0; i < listSentence.length; i++) {
             System.out.print(listSentence[i] + " ");
-            if (listSentence[i].equals(listSentence[listSentence.length - (i + 1)])) {
-                mapIndex.put(i, arrayLisrSentence.size() - i);
-                break;
+            for (int j = listSentence.length - 1; j >= 0; j--) {
+                if (listSentence[i].equals(listSentence[j]) && i < j) {
+                    mapIndex.put(i, j);
+                    break;
+                }
+                mapIndex.put(i, 0);
             }
-            mapIndex.put(i, 0);
         }
 
+        int maxSubString = 0;
+        int theFirstIndexKeyMaxSubString = 0;
+        int theSecondIndexValueMaxSubString = 0;
 
         for (Map.Entry<Integer, Integer> pair : mapIndex.entrySet()) {
             int key = pair.getKey();
             int value = pair.getValue();
             System.out.println(key + " " + value);
+            if (value - key > maxSubString) {
+                maxSubString = value - key;
+                theFirstIndexKeyMaxSubString = key;
+                theSecondIndexValueMaxSubString = value;
+            }
         }
 
-
+        System.out.println(someSentence.substring(theFirstIndexKeyMaxSubString, theSecondIndexValueMaxSubString + 1));
     }
-
-
 }
